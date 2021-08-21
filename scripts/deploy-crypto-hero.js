52ec33b010d0fdeed264b7fb7fe3d036ad346dde
phat.itern <phatNfqAsia>
2021-08-21 12:02:37 +0700

    first commit, no meaning

---
+++ scripts/deploy-crypto-hero.js
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const HerosGame = await hre.ethers.getContractFactory("CryptoHerosGame");
+
+  await HerosGame.deployed();
+  // We get the contract to deploy
+  const HerosToken = await hre.ethers.getContractFactory("CryptoHerosToken");
+
+  await HerosToken.deployed();
+
+  console.log("Heros Game deployed to:", HerosGame.address);
+  console.log("Heros Token deployed to:", HerosToken.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
